0def2e43f367b9566751f727f1afd92de6340c90
csacca <csacca@csacca.net>
2025-06-28 13:19:54 -0400

    Working on pinning kernel commit

--- flake.nix
+++ flake.nix
@@ -3,11 +3,26 @@
 
   # Add all your dependencies here
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    blueprint.url = "github:numtide/blueprint";
-    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    clockworkPiLinux = {
+      # branch == "rpi-6.12.y"
+      url = "github:ak-rex/ClockworkPi-linux/85b196fde0dae6941e54290324e89d57b2d8ab90";
+      flake = false;
+    };
   };
 
   # Load the blueprint
-  outputs = inputs: inputs.blueprint {inherit inputs;};
+  outputs = inputs:
+    inputs.blueprint {
+      inherit inputs;
+      # systems = ["aarch64-linux"];
+    };
 }
